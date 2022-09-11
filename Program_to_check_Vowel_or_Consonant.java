6fb7484a2f5125e42283a6fc7eccc9115c2a6949
Pavandeepak0310 <84892374+Pavandeepak0310@users.noreply.github.com>
2022-09-11 15:27:52 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         char ch = sc.next().charAt(0);         if(ch == 'a' || ch == 'A')             System.out.println("Vowel");         else if(ch == 'e' || ch == 'E')             System.out.println("Vowel");         else if(ch == 'i' || ch == 'I')             System.out.println("Vowel");         else if(ch == 'o' || ch == 'O')             System.out.println("Vowel");         else if(ch == 'u' || ch == 'U')             System.out.println("Vowel");         else             System.out.println("Consonant");         sc.close();     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if(ch == 'a' || ch == 'A')
+            System.out.println("Vowel");
+        else if(ch == 'e' || ch == 'E')
+            System.out.println("Vowel");
+        else if(ch == 'i' || ch == 'I')
+            System.out.println("Vowel");
+        else if(ch == 'o' || ch == 'O')
+            System.out.println("Vowel");
+        else if(ch == 'u' || ch == 'U')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
+        sc.close();
+    }
+}
